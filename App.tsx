7aef98706325f6e760cae9346a1ecee9c1d8918b
Janki Chauhan <jankichauhan0510@gmail.com>
2026-02-10 12:45:57 +0530

    Add Hello component and update App layout for improved structure 10.2.26 12:45PM

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import Hello from './android/app/src/main/components/Hello';
 
 export default function App() {
   return (
-    <View>
-      <Text style={{ fontSize: 30, color: '#fff' }}>App</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Hello />
     </View>
   );
 }
---
+++ android/app/src/main/components/Hello.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export default function Hello() {
+  return (
+    <View>
+      <Text style={{ fontSize: 24, color: '#fff' }}>Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
