d31f410d5f7f3503244345244403bf1f3da8736a
Janki Chauhan <jankichauhan0510@gmail.com>
2026-02-11 12:25:03 +0530

    add simple translation in app.tsx 11.2.26 12:24PM

--- App.tsx
+++ App.tsx
@@ -1,19 +1,74 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import {
+  Alert,
+  Pressable,
+  StyleProp,
+  StyleSheet,
+  Text,
+  View,
+  ViewStyle,
+} from 'react-native';
+import React, { useState } from 'react';
 import Hello from './android/app/src/main/components/Hello';
 import CartScreen from './android/app/src/main/components/CartScreen';
 import ProductScreen from './android/app/src/main/components/ProductScreen';
+import { useTranslation } from 'react-i18next';
+import './assets/i18n/i18n';
 
 export default function App() {
+  const { t, i18n } = useTranslation();
+
+  const [currentLanguage, setLanguage] = useState<string>('en');
+
+  const changeLanguage = (value: string): void => {
+    i18n
+      .changeLanguage(value)
+      .then(() => setLanguage(value))
+      .catch((err: unknown) => console.log(err));
+  };
+
+  const buttonStyle = (lang: string): StyleProp<ViewStyle> => ({
+    backgroundColor: currentLanguage === lang ? '#33A850' : '#d3d3d3',
+    padding: 20,
+  });
+
   return (
+    // <View
+    //   style={{
+    //     flex: 1,
+    //   }}
+    // >
+    //   {/* <Hello /> */}
+    //   <CartScreen />
+    //   <ProductScreen />
+    // </View>
+
     <View
       style={{
         flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
       }}
     >
-      {/* <Hello /> */}
-      <CartScreen />
-      <ProductScreen />
+      <Text style={{ fontWeight: 'bold', fontSize: 25, color: '#33A850' }}>
+        {t('hello')}
+      </Text>
+
+      <Text style={{ fontWeight: 'bold', fontSize: 25, color: '#33A850' }}>
+        {t('janki')}
+      </Text>
+
+      <Text style={{ fontWeight: 'bold', fontSize: 25, color: '#33A850' }}>
+        {t('this line is translated')}
+      </Text>
+
+      <Pressable onPress={() => changeLanguage('en')} style={buttonStyle('en')}>
+        <Text>Select English</Text>
+      </Pressable>
+
+      <Pressable onPress={() => changeLanguage('hi')} style={buttonStyle('hi')}>
+        <Text>हिंदी का चयन करें</Text>
+      </Pressable>
     </View>
   );
 }
